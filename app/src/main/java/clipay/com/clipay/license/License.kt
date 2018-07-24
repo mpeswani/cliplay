95e3225deb5a4b1ce6ceb8fc4eaf4b2efca580a2
Manohar Peswani <peswani.sunny121@gmail.com>
2018-07-25 00:29:24 +0530

    added license

---
+++ app/src/main/java/clipay/com/clipay/license/License.kt
@@ -0,0 +1,52 @@
+package clipay.com.clipay.license
+
+import android.content.Context
+import clipay.com.clipay.util.GradientFileParser
+import com.google.gson.Gson
+import com.marcoscg.licenser.Library
+import com.marcoscg.licenser.License
+import com.marcoscg.licenser.LicenserDialog
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import org.json.JSONArray
+
+
+/**
+ * Created by Manohar Peswani on 24/07/18.
+ * copyright (c) crafty endeavours
+ * manoharpeswani@outlook.com
+ */
+
+
+data class License(val title: String, val url: String, val type: Int)
+
+fun showLicesnse(context: Context) {
+    val result = Single.fromCallable {
+        val inputStream = context.assets.open("license.json")
+        val jsonArray = JSONArray(GradientFileParser.convertStreamToString(inputStream))
+        return@fromCallable Gson().fromJson(jsonArray.toString(), Array<clipay.com.clipay.license.License>::class.java).toList()
+    }.subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : DisposableSingleObserver<List<clipay.com.clipay.license.License>>() {
+                override fun onSuccess(list: List<clipay.com.clipay.license.License>) {
+                    val dialog = LicenserDialog(context)
+                            .setTitle("Licenses")
+                            .setCustomNoticeTitle("Notices for files:")
+                    dialog.setLibrary(Library("Android Support Libraries",
+                            "https://developer.android.com/topic/libraries/support-library/index.html",
+                            License.APACHE))
+                    for (l in list) {
+                        dialog.setLibrary(Library(l.title, l.url, l.type))
+                    }
+                    dialog.setPositiveButton(android.R.string.ok) { _, i ->
+                    }.show()
+                    dispose()
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+
+}
